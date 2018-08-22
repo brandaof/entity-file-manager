db0aa220c22f8a33d8a6616c6f1217f32fe1a5d4
afonso <afonso.rbn@gmail.com>
2018-08-22 09:22:10 -0300

    #

--- src/main/java/org/brandao/entityfilemanager/tx/TransactionEntity.java
+++ src/main/java/org/brandao/entityfilemanager/tx/TransactionEntity.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import org.brandao.entityfilemanager.EntityFile;
 
 public interface TransactionEntity<T, R> 
-	extends  EntityFile<T> {
+	extends EntityFile<T> {
 	
 	void setTransactionStatus(byte value) throws IOException;
 	
