3d73e98e703ae9b654304881f81269be9ffff5a6
afonso <afonso.rbn@gmail.com>
2018-08-29 21:15:29 -0300

    #

---
+++ src/test/java/org/brandao/entityfilemanager/helper/Entity.java
@@ -0,0 +1,35 @@
+package org.brandao.entityfilemanager.helper;
+
+public class Entity {
+
+	private int id;
+	
+	private int status;
+	
+	private String message;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
