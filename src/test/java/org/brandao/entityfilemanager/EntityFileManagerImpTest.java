3df4941d2de4edd05e1069e9d82d4fce7b23ec55
afonso <afonso.rbn@gmail.com>
2018-09-02 23:06:14 -0300

    #

--- src/test/java/org/brandao/entityfilemanager/EntityFileManagerImpTest.java
+++ src/test/java/org/brandao/entityfilemanager/EntityFileManagerImpTest.java
@@ -435,8 +435,8 @@ public class EntityFileManagerImpTest extends TestCase{
 
 	public void testInsertPerformance() throws Throwable{
 		
-		final int task                         = 300;
-		final int ops                          = 1200;
+		final int task                         = 1000;
+		final int ops                          = 1000;
 		final CountDownLatch countDownLatch    = new CountDownLatch(task);
 		final Random random                    = new Random();
 		final List<Throwable> ex               = new ArrayList<Throwable>();
